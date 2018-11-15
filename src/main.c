78597b2a868553a9ca3d877b1df49048c193e97b
fatoumata diallo <fatoumata.diallo7@u-psud.fr>
2018-11-15 20:11:10 +0100

    terminé exo_44

--- src/main.c
+++ src/main.c
@@ -6,10 +6,34 @@
 
 int main(void)
 {
-	short resultat, i=0;
-	while(i<10)
-	{resultat= LancerDe();
-	i++;
-	}
+short int n=0, points=0 ;
+		 n=LancerDe();
+		 
+		 while(!n==1)
+		 {
+		 if(n%2==0);
+		 {
+			 points=points+1 ;
+		 }
+		  else if(n==3)
+		  {
+			 points=points*2 ;
+		 }
+		 else (n==5)
+		 {
+			 if (points=0)
+			 {
+				 points=0 ;
+			 }
+					else 
+			 {
+				 points= points-2;
+			 }
+		 }
+		 }
+				 if (n==1)
+		 {
+			 printf ("fin de tour");
+		 }
     return 0;
 }
