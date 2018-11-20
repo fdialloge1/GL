51b0bbaa662884750c0833cea953427072c1e941
fatoumata diallo <fatoumata.diallo@ge1.iut-cachan.u-psud.fr>
2018-11-20 11:28:16 +0100

    terminé

--- src/main.c
+++ src/main.c
@@ -5,5 +5,21 @@
 
 int main(void)
 {
+	unsigned char bin[10]={0,566,2,3,4,15,45,7,555,101};
+	unsigned int seuil=101  ;
+	unsigned i;
+	for (i=0; i<10; i=i+1)
+	{
+	if (bin[10]<seuil)
+	{
+	 bin[i]=0;	
+	}
+	else
+	{
+		bin[i]=1;
+	}
+	}
+	
     return 0;
-}
+} 
+  
