b48ab407c4de304761edc6f30cf12eb399e13403
fatoumata diallo <fatoumata.diallo@ge1.iut-cachan.u-psud.fr>
2018-11-22 16:42:37 +0100

    terminé exo56

--- src/main.c
+++ src/main.c
@@ -4,26 +4,33 @@
 #include <ansi_c.h>
 #include "my_header.h"
 
+#define SEUILB -100
+#define SEUILH 200
+
 int exo1(void);
 int exo2(void);
 int exo3(void);
-
+int exo4(void);
+int exo5(void);
 int main(void)
 {
 
 	exo1();
 	exo2();
 	exo3();
+    exo4();
+    exo5();
 } 
 
 int exo1(void)
 {
-	unsigned char bin[10]={0,566,2,3,4,15,45,7,555,101};
-	unsigned int seuil=101  ;
-	unsigned i;
+	unsigned char bin[10]={0,56,78,3,60,15,45,7,55,101};
+	unsigned int seuil;
+	unsigned int i;
 	for (i=0; i<10; i=i+1)
 	{
-	if (bin[10]<seuil)
+	 seuil=75;
+	if (bin[i]<seuil)
 	{
 	 bin[i]=0;	
 	}
@@ -38,28 +45,36 @@ int exo1(void)
 int exo2(void)
 {
 	int sat[10]={rand()};
-	signed int SEUILB=-100;
-	unsigned int SEUILH=200;
+
+	unsigned int i;
+
+	for (i=0;i<10;i=i+1)
+		sat[i]=rand()-16383;
 	
-	if (sat[10]>SEUILH)
+
+	for(i=0;i<10;i=i+1)
 	{
-		sat[10]=SEUILH;
+
+	if (sat[i]>SEUILH)
+	{
+		sat[i]=SEUILH;
 	}
-	else if (sat[10]<SEUILB)
+	else if (sat[i]<SEUILB)
 		{
-			sat[10]=SEUILB;
+			sat[i]=SEUILB;
 		}
 		else 
 		{ 
-			sat[10]=sat[10];
+			sat[i]=sat[i];
 		}
-		return 0;
- }
+     }
+    return 0;
+}
 
 int exo3(void)
 {
-	int dist[10]=(1,5,78,6,7,8,4,89,16,48);
-	unsigned i;
+	int dist[10]={1,5,78,6,7,8,4,89,16,48};
+	unsigned int i;
 	
 	for (i=0;i<10;i=i+1)
 	{
@@ -71,6 +86,44 @@ int exo3(void)
 		{
 			dist[i]=dist[i]-dist[i-1];
 		}
-		return 0;
+	
 	}
+		return 0; 
 }
+
+
+int exo4(void)
+{
+	int a[4]={15,6,48,5};
+	int b[4]={16,80,4,7};
+	unsigned int i;
+	unsigned int j;
+	
+	for (i=0;i<4;i=i+1)
+	{
+		j=4-i-1;
+		b[i]=a[j];
+	}
+	return 0;
+}
+
+int exo5(void)
+{
+	int miroir[6]={2,4,7};
+	unsigned i;
+	
+	for (i=0;i<6;i=i+1)
+	{
+		if (i<6/2)
+		{
+			miroir[i]=miroir[i];
+		}
+		else
+		{
+		    miroir[i]=miroir[5-i];	
+		}
+	
+	}
+	return 0;
+}
+
