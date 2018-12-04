be304536f397c9f01075ac23bf9f8287d7066183
fatoumata diallo <fatoumata.diallo@ge1.iut-cachan.u-psud.fr>
2018-12-04 11:42:13 +0100

    termine exo79

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <userint.h>
+#include <ansi_c.h>
 #include "my_header.h"
 
 int main(void)
 {
+	 char phrase[100]= "je m'appelle Lea Dupont et j'ai 19 ans";
+	 char nom[100];
+	 char prenom[100];
+	 unsigned short annaiss;
+	 scanf(phrase,"%s %s %u",nom,prenom,annaiss);
+	 
+	 MessagePopup("Salut",phrase);
     return 0;
 }
